0fcfac944c273b21fcc0107627ddb21ec6711109
James.Hester <jxh@ansto.gov.au>
2016-10-24 09:45:38 +1100

    Output order now understands external categories. Started fixing use of category_key.name instead of category.key_id. Improved star2_to_cif2 routine.

--- pycifrw/Programs/star2_to_cif2.py
+++ pycifrw/Programs/star2_to_cif2.py
@@ -4,17 +4,13 @@ most markedly in their compound data value delimiters (comma and whitespace
 respectively).
 
 We read in the file using STAR2 grammar, then output with CIF2 grammar.
+ 
+Adjust the comment value for your particular uses.
 """
 from CifFile import ReadCif,CifDic
 import time
 
-if __name__ == "__main__":
-    import sys
-    infile = sys.argv[1]
-    if len(sys.argv)>2:
-        outfile = sys.argv[2]
-    else:
-        outfile = infile + '.cif2'
+def do_conversion(infile,outfile):
     startread = time.clock()
     incif = ReadCif(infile,grammar="STAR2")
     print 'Finished reading %s in %f seconds' % (infile,time.clock() - startread)
@@ -25,17 +21,41 @@ if __name__ == "__main__":
     incif.set_grammar("2.0")
     incif.standard = 'Dic'
     incif.SetTemplate("dic_template.dic")
-    comment = """##############################################################################
-#                                                                            #
-#                      DDLm REFERENCE DICTIONARY                             #
-#                                                                            #
-##############################################################################"""
-
-
+    comment = \
+"""###############################################################################
+#                                                                              #
+#                   CIF Dictionary for Modulated Structures                    #
+#                   ---------------------------------------                    #
+#                                                                              #
+#  CIF data definitions specifically for Modulated Structures. These are in    #
+#  addition to those defined in the CIF Core Dictionary version 2.3 (2003).    #
+#                                                                              #
+#  The data included in this dictionary are intended to fulfil the Checklist   #
+#  for the Description of Incommensurate Modulated Crystal Structures,         #
+#  published by the Commission on Aperiodic Crystals. Acta Cryst. (1997),      #
+#  A53, 95-100.                                                                #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#  The DDL1 version of this dictionary was converted to DDLm on 27 June 2014.  #
+#                                                                              #
+################################################################################
+"""
     # print 'Master template:' + `incif.dic_as_cif.master_template`
     print 'check: ' + `incif.recurse_child_list('enumeration')`
     of = open(outfile,"w")
     of.write(incif.WriteOut(comment=comment,saves_after='_description.text'))
     of.close()
     print 'Finished writing %s in %f seconds' % (outfile,time.clock() - startread)
+
+
+if __name__ == "__main__":
+    import sys
+    infile = sys.argv[1]
+    if len(sys.argv)>2:
+        outfile = sys.argv[2]
+    else:
+        outfile = infile + '.cif2'
+
+    do_conversion(infile,outfile)
 
