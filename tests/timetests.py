20c8600ed97b4251c514a89e913a3fb40f88c28d
James.Hester <jxh@ansto.gov.au>
2017-02-28 14:52:02 +1100

    Some adjustments to speed up file input.

--- tests/timetests.py
+++ tests/timetests.py
@@ -1,13 +1,14 @@
 # this simply reports time taken to parse a file
+from __future__ import print_function
 import time
-import CifFile
+from CifFile import ReadCif,Validate,CifDic
 import cProfile
 import pstats
 testfiles = ['1wic.cif','ag03.cif','C13H22O3.cif','../dictionaries/cif_core.dic',
               '../dictionaries/mmcif_pdbx.dic','img2cif_packed.cif']
-#cached_mmdic = CifFile.CifDic('../dictionaries/mmcif_pdbx.dic',standard=None)
-#cached_mmdic = CifFile.CifDic('../dictionaries/mmcif_std_2.0.09.dic',standard=None)
-cached_core = CifFile.CifDic('../dictionaries/cif_core.dic')
+#cached_mmdic = CifDic('../dictionaries/mmcif_pdbx.dic',standard=None)
+#cached_mmdic = CifDic('../dictionaries/mmcif_std_2.0.09.dic',standard=None)
+cached_core = CifDic('../dictionaries/cif_core.dic')
 #valfiles = [ ('ag03.cif',cached_mmdic), 
 #             ('C13H22O3.cif',cached_core)
 valfiles = [
@@ -15,18 +16,18 @@ valfiles = [
 
 for file in testfiles:
     start_time = time.time()
-    jj = CifFile.ReadCif(file,scantype='flex',standard=None)      # no profiling, approx usage
+    jj = ReadCif(file,scantype='flex',standard=None)      # no profiling, approx usage
     finish_time = time.time()
-    print "File %s: wallclock time %8.1f\n" % (file,finish_time - start_time)
-    cProfile.run("jj = CifFile.ReadCif(file,scantype='flex',standard=None) ","profout")
+    print("File %s: wallclock time %8.1f\n" % (file,finish_time - start_time))
+    cProfile.run("jj = ReadCif(file,scantype='flex',standard=None) ","profout")
     p = pstats.Stats( "profout")
     p.strip_dirs().sort_stats("cumulative").print_stats()
     # try to validate  
 for file,dic in valfiles:
     start_time = time.time()
-    jj = CifFile.Validate(file,dic=dic) 
+    jj = Validate(file,dic=dic) 
     finish_time = time.time()
-    print "Validate file %s: %8.1f\n" % (file,finish_time - start_time)
+    print("Validate file %s: %8.1f\n" % (file,finish_time - start_time))
      
 
 
