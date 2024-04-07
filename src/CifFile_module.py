e960cadb9730d07db6dcd512874be9a79cb83e39
Iñigo Gabirondo López <igabirondo13@gmail.com>
2024-04-07 11:34:45 +0200

    Allow constructing CifFile objects from pure strings

--- src/CifFile_module.py
+++ src/CifFile_module.py
@@ -210,8 +210,8 @@ class CifBlock(StarFile.StarBlock):
 
 
 class CifFile(StarFile.StarFile):
-    def __init__(self,datasource=None,strict=1,standard='CIF',**kwargs):
-        super(CifFile,self).__init__(datasource=datasource,standard=standard, **kwargs)
+    def __init__(self,datasource=None,strict=1,standard='CIF',from_str=False,**kwargs):
+        super(CifFile,self).__init__(datasource=datasource,standard=standard, from_str=from_str, **kwargs)
         self.strict = strict
         self.header_comment = \
 """
