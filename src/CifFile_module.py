4662213ba74641c12e7c78f354c80c072c9b5090
Iñigo Gabirondo López <igabirondo13@gmail.com>
2024-04-09 09:16:37 +0200

    Correct get_alternates() function

--- src/CifFile_module.py
+++ src/CifFile_module.py
@@ -2475,6 +2475,8 @@ class CifDic(StarFile.StarFile):
         return {"result":True}        #found alternates
 
     def get_alternates(self,main_name,exclusive_only=False):
+        if self.get(main_name, None) is None:
+            return []
         alternates = self[main_name].get(self.related_func,None)
         alt_names = []
         if alternates != None:
